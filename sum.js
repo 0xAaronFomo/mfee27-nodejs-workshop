faf8096c9b8ca8d996d2ee46e27c16255cbb930b
Aaron <ysjhs00189@gmail.com>
2022-07-30 11:48:22 +0800

    [add]sum.js

---
+++ sum.js
@@ -0,0 +1,11 @@
+function sum(n) {
+    let a = 0;
+    for(i = 0; i<=n ; i++){
+        a += i;
+    }
+    console.log(a);
+  }
+  
+  console.log(sum(1)); // 1
+  console.log(sum(3)); // 6
+  console.log(sum(10)); // 55
