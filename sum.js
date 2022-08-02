95b609c859b050f7deeb9bdd05958ef43952abc7
Aaron <ysjhs00189@gmail.com>
2022-08-02 09:31:04 +0800

    update

--- sum.js
+++ sum.js
@@ -1,11 +1,11 @@
 function sum(n) {
-    let a = 0;
-    for(i = 0; i<=n ; i++){
-        a += i;
-    }
-    console.log(a);
+  let a = 0;
+  for (i = 0; i <= n; i++) {
+    a += i;
   }
-  
-  console.log(sum(1)); // 1
-  console.log(sum(3)); // 6
-  console.log(sum(10)); // 55
+  console.log(a);
+}
+
+console.log(sum(1)); // 1
+console.log(sum(3)); // 6
+console.log(sum(10)); // 55
