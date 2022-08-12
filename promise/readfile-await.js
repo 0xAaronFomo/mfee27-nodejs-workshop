a1f03168e4c699110fecc5a0160c9ffac5374bc3
Aaron Chang <ysjhs00189@gmail.com>
2022-08-13 00:43:12 +0800

    [update]await homework

--- promise/readfile-await.js
+++ promise/readfile-await.js
@@ -1,12 +1,24 @@
-const fs = require('fs');
+const fs = require("fs");
 
 // 記得要放編碼 utf8
 // callback
 // readFile 去硬碟讀檔案，這是很慢的事，他是非同步
-readFile("test.txt", "utf8")
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log(err);
+let readFile = (file, unicode) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, unicode, (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(data);
+    });
   });
+};
+
+async function readfile() {
+  try {
+    console.log(await file("test.txt", "utf8"));
+  } catch (err) {
+    console.log(err);
+  }
+}
+readfile();
