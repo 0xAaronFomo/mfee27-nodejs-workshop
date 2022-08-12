6b78d3e9111b39fe170623a612d5993138e0ee68
Aaron Chang <ysjhs00189@gmail.com>
2022-08-13 00:29:06 +0800

    [create]promise homework

--- promise/readfile-promise.js
+++ promise/readfile-promise.js
@@ -1,11 +1,23 @@
-const fs = require('fs');
+const fs = require("fs");
 
 // 記得要放編碼 utf8
 // callback
 // readFile 去硬碟讀檔案，這是很慢的事，他是非同步
-fs.readFile('test.txt', 'utf8', (err, data) => {
-  if (err) {
-    return console.error('發生錯誤', err);
-  }
-  console.log(data);
-});
+let readFile = (fileName, unicode) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(fileName, unicode, (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(data);
+    });
+  });
+};
+
+readFile("test.txt", "utf8")
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
