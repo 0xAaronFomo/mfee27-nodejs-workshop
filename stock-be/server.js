d814aec8b1d5545afa678b0c3a12ae5d6cea1bf6
Aaron Chang <ysjhs00189@gmail.com>
2022-08-28 13:53:18 +0800

    [update]分頁

--- stock-be/server.js
+++ stock-be/server.js
@@ -1,72 +1,65 @@
-const express = require('express');
-require('dotenv').config();
-const app = express();
+const express = require ('express');
+// 初始化 dotenv
+require('dotenv').config()
 
+
+// 利用 express 這個框架/函式庫 來建立一個 web application
+const app = express();
 const port = process.env.SERVER_PORT;
 
 const cors = require('cors');
 app.use(cors());
 
-// 使用資料庫
+
+//使用資料庫
 const mysql = require('mysql2');
-let pool = mysql
-  .createPool({
+const { response } = require('express');
+let pool = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    // 限制 pool 連線數的上限
     connectionLimit: 10,
-  })
-  .promise();
+    dateStrings: true,
+}).promise()
 
-app.set('view engine', 'pug');
-app.set('views', 'views');
+app.get('/',(req,res) => {
+    res.send('Hello Express!! 這裡是首頁')
+    
+})
 
-app.get('/ssr', (req, res, next) => {
-  res.render('index', {
-    stocks: ['台積電', '長榮航', '聯發科'],
-  });
-});
+app.get('/api/stocks',async (req,res,next) => {
+    let [data] = await pool.execute('SELECT * FROM stocks')
+    
+    res.json(data)
+})
 
-// 一般的 middleware
-app.use((req, res, next) => {
-  console.log('這是中間件 A');
-  let now = new Date();
-  console.log(`有人來訪問喔 at ${now.toISOString()}`);
-  // 一定要寫，讓 express 知道要跳去下一個中間件
-  next();
-});
 
-app.use((req, res, next) => {
-  console.log('這是中間件 C');
-  // 一定要寫，讓 express 知道要跳去下一個中間件
-  next();
-});
+app.get('/api/stocks/:stockId',async(req,res,next) => {
+    const stockId = req.params.stockId
+    let page = req.query.page || 1
+    const perPage = 3
+    let [total] = await pool.execute(`SELECT COUNT(*) AS total FROM stock_prices WHERE stock_id =?`,[stockId])
+    total = total[0].total
+    let lastPage = Math.ceil(total/perPage)
+    const offset = perPage * (page-1)
+    let [data] = await pool.execute('SELECT * FROM stock_prices WHERE stock_id = ? ORDER BY date LIMIT ? OFFSET ? ',[stockId,perPage,offset])
 
-// 路由中間件
-app.get('/', (req, res, next) => {
-  console.log('這裡是首頁');
-  res.send('Hello Express');
-});
-app.get('/test', (req, res, next) => {
-  console.log('這裡是 test 1');
-  res.send('Hello Test 1');
-});
 
-// API
-// 列出所有股票代碼
-app.get('/api/1.0/stocks', async (req, res, next) => {
-  let [data] = await pool.execute('SELECT * FROM stocks');
+    res.json({
+        pagination:{
+            total,
+            perPage,
+            page,
+            lastPage,
+        },
+        data,
+    })
+})
 
-  res.json(data);
-});
 
-app.use((req, res, next) => {
-  console.log('在所有路由中間件的下面 -> 404 了！');
-  res.status(404).send('Not Found!!');
-});
-
-app.listen(port, () => {
-  console.log(`server start at ${port}`);
-});
+app.listen(port,() => {
+    console.log(`server start at ${port}`)
+})
--- stock-fe/src/components/Stock.js
+++ stock-fe/src/components/Stock.js
@@ -1,20 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Stock = () => {
   const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    let getStock = async () => {
+      console.log('stock', 'useEffect []');
+      let response = await axios.get('http://localhost:3001/api/1.0/stocks');
+      setData(response.data);
+    };
+    getStock();
+  }, []);
+
+  useEffect(() => {
+    console.log('stock', 'useEffect[data]');
+    console.log('stock', data);
+  }, [data]);
 
   return (
     <div>
       {error && <div>{error}</div>}
       <h2 className="ml-7 mt-6 text-xl text-gray-600">股票代碼</h2>
 
-      <div className="bg-white bg-gray-50 p-6 rounded-lg shadow hover:shadow-lg m-6 cursor-pointer">
-        <Link to={`/stock/1234`}>
-          <h2 className="text-2xl font-bold mb-2 text-gray-800">1234</h2>
-          <p className="text-gray-700">1234名稱</p>
-        </Link>
-      </div>
+      {data.map((stock, idx) => {
+        return (
+          <div className="bg-white bg-gray-50 p-6 rounded-lg shadow hover:shadow-lg m-6 cursor-pointer">
+            <Link to={`/stock/${stock.id}`}>
+              <h2 className="text-2xl font-bold mb-2 text-gray-800">{stock.id}</h2>
+              <p className="text-gray-700">{stock.name}</p>
+            </Link>
+          </div>
+        );
+      })}
     </div>
   );
 };
